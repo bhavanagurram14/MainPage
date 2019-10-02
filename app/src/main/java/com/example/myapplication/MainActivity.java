9a630534e947df768b3a79e10e154ce0e359aa1f
Gude-Madhuri <gudemadhuri777@gmail.com>
2019-10-02 21:33:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = "GenerateQrCode";
+    String inputValue;
+    ImageView qrimg;
+    EditText edttxt;
+    Button start;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav= findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new Home()).commit();
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment=null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.id1:
+                            selectedFragment = new Home();
+                            break;
+
+                        case R.id.id2:
+                            selectedFragment = new Info();
+                            break;
+
+                        case R.id.id3:
+                            selectedFragment = new Team();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
